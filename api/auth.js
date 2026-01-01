753a98a9be521ee39754e6d878374cce8f438d05
abdo7khashaa <137806033+abdo7khashaa@users.noreply.github.com>
2026-01-01 14:32:25 +0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const key = req.headers["x-app-key"];
+  const key = req.query.key; // ← من الرابط
   if (key !== "AKTV-KEY-2026") {
     return res.status(401).json({ ok: false });
   }
